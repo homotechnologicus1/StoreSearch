5d71cc7fae7c1c1d81fa6983e4dc09971d5fd478
joe <homo.technologicus1@gmail.com>
2020-09-10 21:39:40 +0900

    using a separate boolean variable to keep track of whether a search has been done yet or not

---
+++ StoreSearch/SearchResult.swift
@@ -0,0 +1,14 @@
+//
+//  SearchResult.swift
+//  StoreSearch
+//
+//  Created by joe_mac on 09/10/2020.
+//  Copyright © 2020 Joe K. All rights reserved.
+//
+
+import Foundation
+
+class SearchResult {
+    var name = ""
+    var artistName = ""
+}
